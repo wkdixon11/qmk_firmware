58d73727bc3a283aa09dfb3e9b774d3c8ba68416
Wayne Dixon <wkdixon@yahoo.com>
2022-02-23 15:06:20 -0500

    modified Dactyl layout

--- keyboards/handwired/dactyl_manuform/mini_thumb/keymaps/wkdixon11/keymap.c
+++ keyboards/handwired/dactyl_manuform/mini_thumb/keymaps/wkdixon11/keymap.c
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_mini_thumb( \
   KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,\
-  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                      KC_7,    KC_8,    KC_9,    KC_0,    KC_PMNS, \
+  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                      KC_7,    KC_8,    KC_9,    KC_0,    KC_F12, \
   KC_CAPS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
-  KC_TRNS, KC_TILD, KC_GRV,  _______, _______, _______,                   XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  KC_TRNS, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,                   KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_PIPE, \
                     _______, _______,                                                       _______, _______, \
 		                                  _______, _______, _______, _______, KC_ENT,  _______ \
 ),
@@ -86,10 +86,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT_mini_thumb( \
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
-  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
-  KC_TRNS, KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, KC_PGUP,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_F12, \
-  KC_TRNS, KC_LCBR, KC_RCBR, KC_PIPE, KC_END,  KC_PGDN,                     KC_TRNS, KC_PSCR, KC_INS,  KC_SLCK, KC_PAUS, KC_BSLS, \
+  KC_TRNS, KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                                   KC_F6,  KC_F7,             KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+  KC_TRNS, KC_LCBR,    KC_RCBR,    KC_PIPE,    KC_HOME,    KC_PGUP,                                 KC_NO,  KC_PSCR,           KC_UP,   KC_SLCK, KC_PAUS, KC_F12, \
+  KC_TRNS, KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_END,     KC_PGDN,                                 KC_INS, KC_LEFT,           KC_DOWN, KC_RGHT, KC_ENT,  KC_ENT, \
+  KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_B),                              KC_NO,  C(A(LCA(KC_DEL))), KC_NO,   KC_NO,   KC_NO,   KC_NO, \
                    _______, _______,                                                          _______, _______, \
 		                                  _______,  KC_DEL, _______, _______,  _______,  _______ \
 ),
--- keyboards/sofle/keymaps/wkdixon11/keymap.c
+++ keyboards/sofle/keymaps/wkdixon11/keymap.c
@@ -96,10 +96,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_LOWER] = LAYOUT(
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
-  _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  KC_CAPS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
+  _______,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+  _______,   KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR,  _______,       _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
                        _______, _______, _______, _______, _______,       KC_ENT, _______, _______, _______, _______
 ),
 /* RAISE
@@ -117,11 +117,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT(
-  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  KC_TRNS,  KC_LCBR, KC_RCBR, KC_PIPE,  KC_HOME, KC_PGUP,                        KC_NO, KC_PSCR,   KC_UP, KC_SLCK, KC_PAUS, KC_F12,
-  KC_TRNS, KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_END, KC_PGDN,                       KC_INS, KC_LEFT, KC_DOWN, KC_RGHT,  KC_ENT, KC_ENT,
-  KC_TRNS,LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_B),  KC_TRNS,       KC_TRNS,  KC_NO, C(A(LCA(KC_DEL))), KC_NO, KC_NO,   KC_NO, KC_NO,
-                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  KC_TRNS, KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                                   KC_F6,  KC_F7,             KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  KC_TRNS, KC_LCBR,    KC_RCBR,    KC_PIPE,    KC_HOME,    KC_PGUP,                                 KC_NO,  KC_PSCR,           KC_UP,   KC_SLCK, KC_PAUS, KC_F12,
+  KC_TRNS, KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_END,     KC_PGDN,                                 KC_INS, KC_LEFT,           KC_DOWN, KC_RGHT, KC_ENT,  KC_ENT,
+  KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_B),  KC_TRNS,          KC_TRNS,  KC_NO,  C(A(LCA(KC_DEL))), KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* ADJUST
