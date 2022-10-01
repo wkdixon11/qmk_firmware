da44be96a0e44ad5c67e7fe5869cb366b49e7e93
wkdixon11 <wkdixon@yahoo.com>
2022-09-30 21:04:11 -0400

    updated BM40 keyboard with HRM

--- keyboards/lily58/keymaps/wkdixon11/keymap.c
+++ keyboards/lily58/keymaps/wkdixon11/keymap.c
@@ -7,10 +7,34 @@ enum layer_number {
   _RAISE,
   _ADJUST,
 };
+// Left-hand home row mods
+#define HOME_A LSFT_T(KC_A)
+#define HOME_S LCTL_T(KC_S)
+#define HOME_D LGUI_T(KC_D)
+#define HOME_F LALT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J LALT_T(KC_J)
+#define HOME_K RGUI_T(KC_K)
+#define HOME_L RCTL_T(KC_L)
+#define HOME_SCLN RSFT_T(KC_SCLN)
+
+// Left-hand home row mods Colemak
+#define HOME_CA LSFT_T(KC_A)
+#define HOME_CR LCTL_T(KC_R)
+#define HOME_CS LGUI_T(KC_S)
+#define HOME_CT LALT_T(KC_T)
+
+// Right-hand home row mods Colemak
+#define HOME_CN LALT_T(KC_N)
+#define HOME_CE RGUI_T(KC_E)
+#define HOME_CI RCTL_T(KC_I)
+#define HOME_CO RSFT_T(KC_O)
 
 enum keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK
+  COLEMAK,
+  M0
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -33,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
   KC_EQL,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-  KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_GESC,   HOME_A, HOME_S,  HOME_D,  HOME_F,    KC_G,                     KC_H,  HOME_J,  HOME_K,  HOME_L,  HOME_SCLN, KC_QUOT, \
   KC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
                         KC_LCTL, KC_LGUI, MO(_LOWER), KC_BSPC, KC_SPC, MO(_RAISE), KC_LALT, LCA(KC_DEL) \
 ),
@@ -41,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_COLEMAK] = LAYOUT( \
   KC_EQL,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS, \
-  KC_ESC,   KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
+  KC_GESC,   HOME_CA, HOME_CR, HOME_CS, HOME_CT,    KC_G,                     KC_M, HOME_CN, HOME_CE, HOME_CI, HOME_CO,    KC_QUOT, \
   KC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_LBRC,  KC_RBRC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
                         KC_LCTL, KC_LGUI, MO(_LOWER), KC_BSPC, KC_SPC, MO(_RAISE), KC_LALT, LCA(KC_DEL) \
 ),
